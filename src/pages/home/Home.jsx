711321bba1bc0fef1146eccbf18239ae1ddb82d8
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-11 22:16:47 +0300

    hero content added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import HeroContent from "./components/HeroContent";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <HeroContent />
     </>
   );
 };

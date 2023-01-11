df992008716dc4ae22f244cecb3807ccea20970e
Anas Firly <anasfirly20@gmail.com>
2023-01-11 20:21:33 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-96">
+      <div className="flex flex-col">
+        <div>FOOTER</div>
+        <div>FOOTER</div>
+        <div>FOOTER</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

548314e6ec4019a4d62b1ff75a3ed45ae9d715e2
Ilham Akbar <milham.nj25@gmail.com>
2023-01-11 18:15:06 +0300

    added text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import OurWork from "./components/navbar/OurWork";
 import Home from "./pages/home/Home";
+// import OurWork from "./pages/home/OurWork";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <OurWork />
     </>
   );
 }
---
+++ src/components/navbar/OurWork.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const OurWork = () => {
+  return (
+    <div className='w-full my-14   '>
+        <div className='max-w-[1240px] mx-auto '>
+            <div className='text-center'>
+            <h1 className='text-slate-900/80  text-6xl font-bold   '>Our Work </h1> 
+       <p className='text-blue-500/90 font-poppins text-xl font-semibold my-2 '>Taking of to a sky, where creativity is limitless </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default OurWork

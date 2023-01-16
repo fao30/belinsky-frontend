1981e81fda07b88c7604f9120367250a0b39217c
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-17 00:53:02 +0300

    added route to project detail: menantea & abnon

--- src/pages/our-work/belinsky-studio/components/CardOverlay.jsx
+++ src/pages/our-work/belinsky-studio/components/CardOverlay.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 const CardOverlay = ({ src, h3, p }) => {
+  const navigate = useNavigate();
   return (
     <div className="h-[20rem] w-full sm:w-[28rem] lg:w-[30%] bg-slate-400 relative group rounded-[1rem]">
       <img
@@ -7,7 +10,16 @@ const CardOverlay = ({ src, h3, p }) => {
         className="absolute object-cover w-full h-full rounded-[1rem]"
       />
       <div className="group-hover:opacity-0 transition-all duration-300 absolute h-full w-full bg-[#0225818C] text-white rounded-[1rem]">
-        <div className="flex flex-col items-center pt-48 pl-7 justify-start w-full h-full text-left text-white">
+        <div
+          onClick={() =>
+            navigate(
+              `${h3 === "Abang None Jakarta Timur" ? "/ourwork/abnon" : ""}`
+            )
+          }
+          className={`flex flex-col items-center pt-48 pl-7 justify-start w-full h-full text-left text-white ${
+            h3 === "Abang None Jakarta Timur" ? "cursor-pointer" : ""
+          }`}
+        >
           <h3 className="text-[20px] font-semibold text-white w-full">{h3}</h3>
           <p className="w-full pr-6 drop-shadow-md text-[14px]">{p}</p>
         </div>

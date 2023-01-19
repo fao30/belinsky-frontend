7cd9160cbe14f8cc950b014298421b1aa7b8971c
Alvin Pratama <palveeen22@gmail.com>
2023-01-19 23:15:50 +0300

    edited about part 1

--- src/pages/about/components/Visi.jsx
+++ src/pages/about/components/Visi.jsx
@@ -3,13 +3,13 @@ import img1 from "../../about/assets/img1_1.png";
 
 export const Visi = () => {
   return (
-    <section className="w-full min-h-screen">
-      <h1 className="text-[75px] font-bold text-[#022581] text-center p-[2vw]">
+    <section className="w-full">
+      <h1 className="lg:text-[75px] text-[40px] font-bold text-[#022581] text-center p-[2vw]">
         Taking off to a sky, where creativity is limitless.
       </h1>
 
       {/* image on visi*/}
-      <div className="hidden w-full h-full md:flex md:justify-start">
+      <div className="w-full h-full md:flex md:justify-start">
         <div className="flex justify-center items-start h-full">
           <div className="self-start">
             <img
@@ -23,8 +23,10 @@ export const Visi = () => {
 
       {/* title description */}
       <div className="flex flex-col self-center w-full p-[2vw] text-left">
-        <h1 className="text-[60px] font-bold text-[#022581]">OUR VISION</h1>
-        <h2 className="text-wrap text-[36px]  [word-spacing:4px] text-[#022581] w-full">
+        <h1 className="lg:text-[60px] text-[30px] font-bold text-[#022581]">
+          OUR VISION
+        </h1>
+        <h2 className="text-wrap lg:text-[36px] text-[18px]  [word-spacing:4px] text-[#022581] w-full">
           Our vision is to become a home for filmmakers/creators to create and
           share those that have constructive values and can fulfill one of the
           needs of the community, especially in the entertainment industry.

7c58536a7c003bb225cee64a3a300e280b4639d6
Anas Firly <anasfirly20@gmail.com>
2023-01-21 22:40:59 +0300

    adjusted text size of button cards in homepage

--- src/pages/home/WhatWeOffer/production-house/components/ButtonCard.jsx
+++ src/pages/home/WhatWeOffer/production-house/components/ButtonCard.jsx
@@ -2,12 +2,12 @@ import { Icon } from "@iconify/react";
 
 const ButtonCard = ({ icon, name }) => {
   return (
-    <div className="flex items-center justify-center p-3 w-[31%] font-bold bg-white rounded-lg shadow-xl">
+    <div className="flex items-center justify-center p-2 w-[31%] font-bold bg-white rounded-lg shadow-xl">
       <div className="flex items-center gap-3">
         <div className="sticky">
           <Icon icon={icon} width="25" />
         </div>
-        <div className="text-sm">{name}</div>
+        <div className="text-sm font-bold md:text-lg">{name}</div>
       </div>
     </div>
   );

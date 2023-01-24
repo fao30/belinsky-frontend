e0f0e24a128cb27891075bcdac042f5245411773
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-01-24 23:40:38 +0300

    route * added

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/ourwork" element={<OurWork />} />
         <Route path="/ourwork/menantea" element={<Menantea />} />

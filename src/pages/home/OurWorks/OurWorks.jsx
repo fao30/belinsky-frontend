ef83e7e5371444fbaf43a10932aba0223c177c21
Anas Firly <anasfirly20@gmail.com>
2023-01-24 17:24:00 +0300

    adjusted borders

--- src/pages/home/OurWorks/OurWorks.jsx
+++ src/pages/home/OurWorks/OurWorks.jsx
@@ -5,7 +5,7 @@ import ProductionHouse from "./ProductionHouse";
 const OurWorks = () => {
   return (
     <>
-      <h1 className="text-white text-center text-4xl lg:text-6xl bg-[#0225818C] p-5 pb-2 my-[5vh] md:my-[10vh]">
+      <h1 className="text-white text-center text-4xl md:text-5xl lg:text-6xl bg-[#0225818C] p-5 pb-2 md:p-6 lg:p-8 my-[5vh] md:my-[10vh]">
         Our Works
       </h1>
       <ProductionHouse />

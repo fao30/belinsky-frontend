162823defc057f83e41812d6a3ad26bc0a4e87a5
Anas Firly <anasfirly20@gmail.com>
2023-01-26 22:23:52 +0300

    adjusted cards animation

--- src/pages/about/phouse-bstudio/components/BStudio.jsx
+++ src/pages/about/phouse-bstudio/components/BStudio.jsx
@@ -17,17 +17,17 @@ const BStudio = () => {
           </p>
         </div>
         <div className="px-8 flex w-[100%] md:w-[90%] lg:-[70%] xl:w-[50%] h-full items-end gap-5">
-          <div className="flex flex-col w-[50%] items-end gap-5">
+          <div className="flex flex-col w-[50%] items-end gap-5 z-10">
             <CardHeroBS
               src={bh1}
-              className="self-start object-cover transition duration-700 ease-in-out translate-x-16 translate-y-24 max-sm:group-hover:-translate-y-0 rounded-2xl grayscale hover:grayscale-0 max-xl:translate-y-48 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:delay-300 group-hover:duration-700 group-hover:ease-in-out"
+              className="self-start object-cover transition duration-700 ease-in-out translate-x-16 md:translate-x-32 rounded-2xl grayscale hover:grayscale-0 group-hover:translate-x-0"
               blurPic="LAGuK_-;9^00D4xY4:IU00$~-;_N"
             />
           </div>
           <div className="w-[50%]">
             <CardHeroBS
               src={bh2}
-              className="object-cover transition duration-700 ease-in-out -translate-x-20 max-sm:group-hover-translate-y-0 max-sm:translate-y-12 -translate-y-28 rounded-2xl grayscale hover:grayscale-0 group-hover:translate-x-0 max-xl:translate-y-12 group-hover:translate-y-0 group-hover:delay-200 group-hover:duration-700 group-hover:ease-in-out"
+              className="object-cover transition duration-700 ease-in-out -translate-x-16 md:-translate-x-28 rounded-2xl grayscale hover:grayscale-0 group-hover:translate-x-0"
               blurPic="LBIEqi8^9ut5_NxU%Mt64;WUjEIV"
             />
           </div>

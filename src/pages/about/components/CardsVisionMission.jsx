f9dcef0e5ad9f0032d83e92bceb5c24afdffdefa
Anas Firly <anasfirly20@gmail.com>
2023-01-31 05:08:53 +0300

    updated blurhash

--- src/pages/about/components/CardsVisionMission.jsx
+++ src/pages/about/components/CardsVisionMission.jsx
@@ -7,31 +7,39 @@ const CardsVisionMission = ({ src, h1, p, blurPic }) => {
 
   return (
     <>
-      <div className="w-full md:w-[90%] lg:w-[80%] xl:w-[65%] h-full lg:mx-[8vw]">
-        <div className="relative">
-          <LazyLoadImage
-            loading="lazy"
-            alt=""
-            src={src}
-            beforeLoad={() => setIsLoading(true)}
-            afterLoad={() => setIsLoading(false)}
-            className="w-full h-full object-cover transition duration-300 ease-in-out grayscale hover:grayscale-0"
+      <div className="w-full md:w-[90%] lg:w-[80%] xl:w-[65%] h-full lg:mx-[8vw] relative">
+        {/* <div className="relative"> */}
+        <LazyLoadImage
+          loading="lazy"
+          alt=""
+          src={src}
+          beforeLoad={() => setIsLoading(true)}
+          afterLoad={() => setIsLoading(false)}
+          className="object-cover w-full h-full transition duration-300 ease-in-out grayscale hover:grayscale-0"
+        />
+        {isLoading ? (
+          <BlurhashCanvas
+            hash={blurPic}
+            className="absolute top-0 object-cover w-full h-full transition duration-300 ease-in-out grayscale hover:grayscale-0"
           />
-          {isLoading ? (
-            <BlurhashCanvas
-              hash={blurPic}
-              className="absolute top-0 object-cover w-full h-full transition duration-300 ease-in-out grayscale hover:grayscale-0"
-            />
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="flex justify-center max-md:px-[4vw] max-md:py-[4vw] px-8 pt-8">
+        ) : (
+          <>
+            {" "}
+            <div className="flex justify-center max-md:px-[4vw] max-md:py-[4vw] px-8 pt-8">
+              <div className="flex flex-col">
+                <h1 className="uppercase aboutTitleSize">{h1}</h1>
+                <p className="mt-1 aboutSize">{p}</p>
+              </div>
+            </div>
+          </>
+        )}
+        {/* </div> */}
+        {/* <div className="flex justify-center max-md:px-[4vw] max-md:py-[4vw] px-8 pt-8">
           <div className="flex flex-col">
-            <h1 className="  uppercase aboutTitleSize">{h1}</h1>
+            <h1 className="uppercase aboutTitleSize">{h1}</h1>
             <p className="mt-1 aboutSize ">{p}</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );

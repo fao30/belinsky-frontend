22bba2353ba37cdd32f6b3b31fea224ede3078e2
Anas Firly <anasfirly20@gmail.com>
2023-01-31 11:35:07 +0300

    adjusted arrows / removed unnecessary packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    // defaults to these values
-    textFillColor: (theme) => theme("borderColor"),
-    textStrokeColor: (theme) => theme("borderColor"),
-    textStrokeWidth: (theme) => theme("borderWidth"),
-    paintOrder: {
-      fsm: { paintOrder: "fill stroke markers" },
-      fms: { paintOrder: "fill markers stroke" },
-      sfm: { paintOrder: "stroke fill markers" },
-      smf: { paintOrder: "stroke markers fill" },
-      mfs: { paintOrder: "markers fill stroke" },
-      msf: { paintOrder: "markers stroke fill" },
-    },
-    variants: {
-      // all the following default to ['responsive']
-      textFillColor: ["responsive"],
-      textStrokeColor: ["responsive"],
-      textStrokeWidth: ["responsive"],
-      paintOrder: ["responsive"],
-    },
     fontFamily: {
       poppins: ["Poppins"],
       nanami: ["nanami"],
@@ -28,10 +9,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [
-    require("tailwindcss-text-fill-stroke")(),
-    require("@tailwindcss/line-clamp"),
-    require("tailwind-scrollbar-hide"),
-    require("daisyui"),
-  ],
+  plugins: [require("daisyui")],
 };

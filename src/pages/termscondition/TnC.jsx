1556e858b4e6eb39bcc18a0c3f180da6c5127155
Anas Firly <anasfirly20@gmail.com>
2023-02-01 18:43:12 +0300

    adjusted ABC to font-bold for terms&conditions

--- src/pages/termscondition/TnC.jsx
+++ src/pages/termscondition/TnC.jsx
@@ -90,38 +90,44 @@ const TnC = () => {
     >
       {/* TnC  */}
       <section className="text-[#022581] h-full w-full">
-        <h1 className="   text-center text-4xl md:text-5xl lg:text-6xl mb-16 md:mb-24">
+        <h1 className="mb-16 text-4xl text-center md:text-5xl lg:text-6xl md:mb-24">
           TERMS & CONDITIONS
         </h1>
         <div className="flex flex-col gap-12 lg:px-[12vw]">
           <div>
-            <h1 className="text-xl md:text-2xl">A. Sebelum Booking Studio</h1>
-            <ol className="list-decimal pl-7 md:pl-14 mt-3">
+            <h1 className="text-xl font-bold md:text-2xl">
+              A. Sebelum Booking Studio
+            </h1>
+            <ol className="mt-3 list-decimal pl-7 md:pl-14">
               {ASebelumBook?.map((e) => {
-                return <li className="   text-lg md:text-xl">{e?.li}</li>;
+                return <li className="text-lg md:text-xl">{e?.li}</li>;
               })}
             </ol>
           </div>
           <div>
-            <h1 className="text-xl md:text-2xl">B. Penggunaan Studio</h1>
-            <ol className="list-decimal pl-7 md:pl-14 mt-3">
+            <h1 className="text-xl font-bold md:text-2xl">
+              B. Penggunaan Studio
+            </h1>
+            <ol className="mt-3 list-decimal pl-7 md:pl-14">
               {BPenggunaanStudio?.map((e) => {
-                return <li className="   text-lg md:text-xl">{e?.li}</li>;
+                return <li className="text-lg md:text-xl">{e?.li}</li>;
               })}
             </ol>
           </div>
           <div>
-            <h1 className="text-xl md:text-2xl">C. Setelah Pemakaian Studio</h1>
-            <ol className="list-decimal pl-7 md:pl-14 mt-3">
+            <h1 className="text-xl font-bold md:text-2xl">
+              C. Setelah Pemakaian Studio
+            </h1>
+            <ol className="mt-3 list-decimal pl-7 md:pl-14">
               {CPenggunaanStudio?.map((e) => {
-                return <li className="   text-lg md:text-xl">{e?.li}</li>;
+                return <li className="text-lg md:text-xl">{e?.li}</li>;
               })}
             </ol>
           </div>
         </div>
         {/* COVID 19 */}
-        <section className="   text-lg md:text-xl">
-          <h1 className="   text-center text-4xl md:text-5xl lg:text-6xl mt-24 mb-12 lg:mb-24 lg:mt-48">
+        <section className="text-lg md:text-xl">
+          <h1 className="mt-24 mb-12 text-4xl text-center md:text-5xl lg:text-6xl lg:mb-24 lg:mt-48">
             New Normal Protocol Related to COVID-19
           </h1>
           <div className="flex  flex-col gap-3 lg:px-[12vw]">
@@ -130,7 +136,7 @@ const TnC = () => {
             </p>
             <ol className="list-decimal pl-7 md:pl-14">
               {NewNormalProtocol?.map((e) => {
-                return <li className="   text-lg md:text-xl">{e?.li}</li>;
+                return <li className="text-lg md:text-xl">{e?.li}</li>;
               })}
             </ol>
             <p>

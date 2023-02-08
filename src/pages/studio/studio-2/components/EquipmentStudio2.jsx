b263c65e477ff6423c44083f25d43f8a08ae8605
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-02-08 16:46:27 +0300

    pricelist adjusted

--- src/pages/studio/studio-2/components/EquipmentStudio2.jsx
+++ src/pages/studio/studio-2/components/EquipmentStudio2.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const EquipmentStudio2 = () => {
   return (
     <div className="px-[6vw] flex justify-center">
-      <div className="max-sm:p-6 px-12 py-8 w-full md:w-[60%] lg:w-[50%] xl:w-[30%] border-[2px] border-[#022581] rounded-xl bg-white drop-shadow-[0_1px_1px_black]">
+      <div className="max-sm:p-6 px-12 py-8 w-full md:w-[60%] lg:w-[50%] xl:w-[35%] border-[2px] border-[#022581] rounded-xl bg-white drop-shadow-[0_1px_1px_black]">
         <div className="flex flex-col">
           <div className="w-full text-[#022581] text-xl lg:text-2xl">
             <div className="flex flex-col">
@@ -15,10 +15,10 @@ const EquipmentStudio2 = () => {
               <h3 className="mt-6 mb-2">Background Choices</h3>
               <p>White, Black, Grey, Yellow</p>
               <div className="mt-2 flex flex-row drop-shadow-[2px_2px_2px_gray]">
-                <div className="bg-white h-2 w-12" />
-                <div className="bg-black h-2 w-12" />
-                <div className="bg-[#ADADAD] h-2 w-12" />
-                <div className="bg-[#FFEF00] h-2 w-12" />
+                <div className="bg-white h-2 w-[4.5rem]" />
+                <div className="bg-black h-2 w-[4.5rem]" />
+                <div className="bg-[#ADADAD] h-2 w-[4.5rem]" />
+                <div className="bg-[#FFEF00] h-2 w-[4.5rem]" />
               </div>
               <h3 className="mt-6 mb-2">Additional</h3>
               <ul>

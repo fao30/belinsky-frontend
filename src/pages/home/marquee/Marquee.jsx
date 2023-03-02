c9a70b9c5a20028b2a3aaf79c09602e3b414feac
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-03-03 02:06:12 +0300

    code cleaned up

--- src/pages/home/marquee/Marquee.jsx
+++ src/pages/home/marquee/Marquee.jsx
@@ -59,11 +59,11 @@ const MarqueeSection = () => {
           className="bg-gradient-to-t from-[#D9D9D9] via-[#D9D9D9] to-transparent"
         >
           {dataMarquee.map(({ img }) => (
-            <div className="mx-7 lg:mx-14">
+            <div className="mx-8 lg:mx-16">
               <img
-                alt="Belinsky Studio Menantea Traveloka IndiHome Pluang+ Kumaw Ramen Jerome Polin "
+                alt="Belinsky Studio"
                 src={img}
-                className="w-[8rem] lg:w-[10rem] custom-marq"
+                className="w-[8rem] lg:w-[10rem] custom-marq "
               />
             </div>
           ))}
